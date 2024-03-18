4ff0422958429bdb8fbf3441fbdfc88964ffdebc
MindOverflow <51059906+PettingStrings@users.noreply.github.com>
2024-03-18 19:14:30 +0100

    Spring server initial commit

---
+++ geospotter.server/src/main/java/com/camerino/ids/fps/geospotter/server/GeospotterServerApplication.java
@@ -0,0 +1,13 @@
+package com.camerino.ids.fps.geospotter.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeospotterServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeospotterServerApplication.class, args);
+	}
+
+}
