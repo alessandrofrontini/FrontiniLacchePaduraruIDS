9c88c6b6fab5bd33f56c8adc71bbd3167207f831
MindOverflow <51059906+PettingStrings@users.noreply.github.com>
2024-04-19 16:49:17 +0200

    Typo

--- app/geospotter/client/src/main/java/com/camerino/ids/fps/client/Main.java
+++ app/geospotter/client/src/main/java/com/camerino/ids/fps/client/Main.java
@@ -4,6 +4,6 @@ import static javafx.application.Application.launch;
 
 public class Main {
     public static void main(String[] args) {
-        HelloApplication.main(new String[]{});
+        HelloApplication.main(args);
     }
 }
