f2dfc40355226c10cd140cbabe1b18ef3774d00f
MindOverflow <51059906+PettingStrings@users.noreply.github.com>
2024-04-22 19:41:48 +0200

    Update ClsMenuCuratore.java

--- app/geospotter/cli/src/main/java/com/camerino/cli/menu/ClsMenuCuratore.java
+++ app/geospotter/cli/src/main/java/com/camerino/cli/menu/ClsMenuCuratore.java
@@ -3,6 +3,6 @@ package com.camerino.cli.menu;
 public class ClsMenuCuratore implements IMenu{
     @Override
     public void menu() {
-        //TODO: implementare
+        //TODO: implementare 
     }
 }
