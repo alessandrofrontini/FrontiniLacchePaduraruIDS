e037a60f5aa2f7f61da08efafcfea4b1992c98b1
MindOverflow <51059906+PettingStrings@users.noreply.github.com>
2024-04-26 18:00:28 +0200

    Import Core in JavaFX

--- app/geospotter/client/src/main/java/module-info.java
+++ app/geospotter/client/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.camerino.ids.fps.client {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires com.camerino.ids.core;
 
     opens com.camerino.ids.fps.client to javafx.fxml;
     exports com.camerino.ids.fps.client;
