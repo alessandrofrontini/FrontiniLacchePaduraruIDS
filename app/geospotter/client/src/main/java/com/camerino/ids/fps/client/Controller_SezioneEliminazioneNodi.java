ca0ca19b3cd1f1e383405147c2d727fa663bb156
SaverioLacche__ <francescosave.lacche@studenti.unicam.it>
2024-04-29 21:54:57 +0200

    -

--- app/geospotter/client/src/main/java/com/camerino/ids/fps/client/Controller_SezioneEliminazioneNodi.java
+++ app/geospotter/client/src/main/java/com/camerino/ids/fps/client/Controller_SezioneEliminazioneNodi.java
@@ -1,4 +1,81 @@
 package com.camerino.ids.fps.client;
 
+<<<<<<< HEAD
 public class Controller_SezioneEliminazioneNodi {
+=======
+
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+public class Controller_SezioneEliminazioneNodi implements Initializable
+{
+    @FXML
+    Button sezioneEliminazioneNodiBTNConferma,sezioneEliminazioneNodiBTNhome;
+
+    @FXML
+    TableView sezioneEliminazioneNodiElenco;
+
+    public void eliminaNodo()
+    {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        ArrayList<String> test = new ArrayList<String>();
+
+        test.add("NODO 1");
+        test.add("NODO 2");
+
+        for(int i = 0; i<test.size();i++)
+        {
+
+        }
+
+    }
+
+    //region Navigazione
+    public void navigateToSezioneNavigazione(MouseEvent mouseEvent) {
+        this.SwitchScene("SezioneVisualizzazione.fxml",mouseEvent);
+    }
+
+    //region Navigazione - Metodi Privati
+    private void SwitchScene (String nomeScena, MouseEvent mouseEvent)
+    {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(nomeScena)));
+            //Utilities grafiche
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    //endregion
+
+    //endregion
+>>>>>>> parent of dec0096 (.)
 }
--- app/geospotter/client/src/main/java/com/camerino/ids/fps/client/Controller_SezioneInserimentoNodi.java
+++ app/geospotter/client/src/main/java/com/camerino/ids/fps/client/Controller_SezioneInserimentoNodi.java
@@ -1,6 +1,95 @@
 package com.camerino.ids.fps.client;
 
+<<<<<<< HEAD
 public class Controller_SezioneInserimentoNodi
 {
 
+=======
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class Controller_SezioneInserimentoNodi
+{
+    @FXML
+    TextField sezioneInserimentoNodiNome,sezioneInserimentoNodiComuneAssociato,sezioneInserimentoNodiPosizioneX,sezioneInserimentoNodiPosizioneY,sezioneInserimentoNodiDescrizione,sezioneInserimentoNodiDataInizio,sezioneInserimentoNodiDataFine;
+    @FXML
+    CheckBox sezioneInserimentoNodiIsTemporizzato;
+
+    public void inserisciNodo()
+    {
+        String Nome = this.getNameFromTextField(sezioneInserimentoNodiNome);
+        String ComuneAssociato = this.getNameFromTextField(sezioneInserimentoNodiComuneAssociato);
+        String PosizioneX = this.getNameFromTextField(sezioneInserimentoNodiPosizioneX);
+        String PosizioneY = this.getNameFromTextField(sezioneInserimentoNodiPosizioneY);
+        String Descrizione = this.getNameFromTextField(sezioneInserimentoNodiDescrizione);
+
+        if(this.sezioneInserimentoNodiIsTemporizzato.isSelected())
+            {
+                String DataInizio = this.getNameFromTextField(sezioneInserimentoNodiDataInizio);
+                String DataFine = this.getNameFromTextField(sezioneInserimentoNodiDataFine);
+                boolean isTemporizzato = true;
+            }
+
+        //Creazione oggetto?
+
+
+    }
+    //region Navigazione
+    public void navigateToSezioneNavigazione(MouseEvent mouseEvent) {
+        this.SwitchScene("SezioneVisualizzazione.fxml",mouseEvent);
+    }
+
+    //region Navigazione - Metodi Privati
+    private void SwitchScene (String nomeScena, MouseEvent mouseEvent)
+    {
+        try {
+            Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(nomeScena)));
+            //Utilities grafiche
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setResizable(false);
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    //endregion
+
+    //endregion
+
+    //region Utilities
+    /**
+     * Metodo di comodo che elimina il contenuto di una textArea
+     * @param t textArea
+     */
+    private void clearTextFromTextField(TextField t)
+    {
+        t.clear();
+    }
+
+    /**
+     * Metodo di comodo che estrae il contenuto da una textField
+     * @param tf textField
+     * @return Contenuto
+     */
+    private String getNameFromTextField (TextField tf)
+    {
+        String name = tf.getText();
+        tf.clear();
+        return name;
+    }
+    //endregion
+>>>>>>> parent of dec0096 (.)
 }
