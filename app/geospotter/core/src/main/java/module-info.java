caf96c26569dd1a787ea5e51d783817fa02016e5
PettingStrings <danutzzu2002.ro@gmail.com>
2024-06-13 15:04:26 +0200

    Fix Import

--- app/geospotter/core/src/main/java/module-info.java
+++ app/geospotter/core/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.camerino.ids.core {
     requires com.fasterxml.jackson.annotation;
     exports com.camerino.ids.core.data.azioni;
     exports com.camerino.ids.core.data.contenuti;
-    exports com.camerino.ids.core.data.punteggio;
     exports com.camerino.ids.core.data.segnalazioni;
     exports com.camerino.ids.core.data.utenti;
     exports com.camerino.ids.core.data.utils;
